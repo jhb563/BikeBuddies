54a647b778c9d146318f0945128b758b5bf30fa4
Sam Echevarria <sechevarria@hmc.edu>
2015-03-30 18:10:03 -0700

    layouts  (mostly) done
    did xml and general look, need to connect screens

--- BikeBuddies/app/src/main/java/com/example/androidsgv/bikebuddies/LeaderboardScreen.java
+++ BikeBuddies/app/src/main/java/com/example/androidsgv/bikebuddies/LeaderboardScreen.java
@@ -4,14 +4,35 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 
 public class LeaderboardScreen extends ActionBarActivity {
+
+    ListView list;
+    String[] people ={
+            "Hannah Y.",
+            "Jim B.",
+            "James B.",
+            "Vanessa R.",
+            "George S."
+    };
+    String[] data ={
+            "04:05:10 7.5mi",
+            "03:04:12 2.2mi",
+            "03:01:01 2.1mi",
+            "02:34:19 1.8mi",
+            "00:00:00 0.0mi"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard_screen);
+
+        ClassLeaderboardCustomAdapter adapter=new ClassLeaderboardCustomAdapter(this, people, data);
+        list=(ListView)findViewById(R.id.listViewForLeaderboard);
+        list.setAdapter(adapter);
     }
 
 
