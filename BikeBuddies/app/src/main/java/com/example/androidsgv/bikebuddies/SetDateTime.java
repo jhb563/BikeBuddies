6db23ed5c5c3834da7e1ddad7d666f695ca5501a
Hannah Young <hyoung@hmc.edu>
2015-04-01 21:26:43 -0700

    dialog added

--- BikeBuddies/app/src/main/java/com/example/androidsgv/bikebuddies/SetDateTime.java
+++ BikeBuddies/app/src/main/java/com/example/androidsgv/bikebuddies/SetDateTime.java
@@ -1,28 +1,35 @@
 package com.example.androidsgv.bikebuddies;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
 
 
-public class SetDateTime extends ActionBarActivity {
+public class SetDateTime extends ActionBarActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_date_time);
-
     }
 
 
@@ -98,6 +105,59 @@ public class SetDateTime extends ActionBarActivity {
             // Do something with the date chosen by the user
         }
     }
+
+    public static class AlertDialogFragment extends DialogFragment {
+        @Override
+        // Creates the yes no dialog
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.alert_message)
+                    .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // FIRE ZE MISSILES! Or, you know, confirm that yes, the user does
+                            // want to do this
+                            DialogFragment newFragment = new confirmDialogFragment();
+                            newFragment.show(getFragmentManager(), "confirmed");
+                        }
+                    })
+                    .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+    }
+
+
+
+    public static class confirmDialogFragment extends DialogFragment {
+        @Override
+        // creates the confirmation dialog fragment
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.confirm)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // FIRE ZE MISSILES! Or, you know, confirm that yes, the user does
+                            // want to do this
+
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+    }
+
+    public void yesNoAlert(View view) {
+        DialogFragment newFragment = new AlertDialogFragment();
+        newFragment.show(getFragmentManager(), "alert");
+    }
+
+
 }
 
 
